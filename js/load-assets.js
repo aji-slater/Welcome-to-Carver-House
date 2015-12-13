21e986ec2437adcb75cc142542e5fb62d2a4c389
[01;31m[K"name"[m[K: "Christopher \"Aji\" Slater <[01;31m[K"email"[m[K: null>
2015-12-12 18:33:19 -0600

    added larger board tile generation

---
+++ js/load-assets.js
@@ -0,0 +1,8 @@
+
+var loadAll = function() {
+  game.load.image('tile', 'assets/tiles/wood-floor-tile.png');
+  game.load.image('necklace', 'assets/tiles/wood-floor-tile.png');
+  game.load.spritesheet('player', 'assets/figure.png', 50, 50);
+  game.load.spritesheet('debugTile', 'assets/tiles/debug_tiles.png', 100, 50);
+
+}

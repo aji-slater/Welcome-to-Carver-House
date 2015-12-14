107446e0231a23be0a2ff62c91f89747f1cf2fc8
Troy Kuziel <leizuk777@gmail.com>
2015-12-14 16:10:43 -0600

    Add pause features that are broken, add player perish sprite movement image

--- js/PauseMenu.js
+++ js/PauseMenu.js
@@ -6,27 +6,34 @@ function pauseGame() {
     */
 
     // Create a label to use as a button
-    pause_label = this.game.add.text(80, 5, 'Pause', { font: '24px Arial', fill: '#fff' });
+    pause_label = this.game.add.text(width - 100, 5, 'pause', { font: '24px Arial', fill: '#fff' });
     pause_label.fixedToCamera = true;
     pause_label.inputEnabled = true;
-    pause_label.events.onInputDown.add(function () {
+    pause_label.events.onInputUp.add(function () {
         // When the pause button is pressed, we pause the game
-        this.game.paused = true;
+        this.game.world.isPaused = true;
+        // this.game.input.disabled = true;
+        //
+        // this.game.physics.gravity.y = 0;
 
-        // Then add the menu
-        menu = this.game.add.sprite(width/2, height/2, 'menu');
+        menu = this.game.add.sprite(700, 300, 'menu');
+        menu.fixedToCamera = true;
         menu.anchor.setTo(0.5, 0.5);
 
+
+        // Then add the menu
+
         // And a label to illustrate which menu item was chosen. (This is not necessary)
-        chooseLabel = this.game.add.text(width/2, height-150, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
-        chooseLabel.anchor.setTo(0.5, 0.5);
+        // chooseLabel = this.game.add.text(width/2, height-150, 'Click outside menu to continue', { font: '30px Arial', fill: '#fff' });
+        // chooseLabel.anchor.setTo(0.5, 0.5);
     });
 
     // Add a input listener that can help us return from being paused
-    this.game.input.onDown.add(unPause, self);
+    this.game.input.onDown.add(unpause, self);
+    // pause_label.events.onDown.add(unPause, self);
 
     // And finally the method that handels the pause menu
-    function unPause(event){
+    function unpause(event){
         // Only act if paused
         if(this.game.paused){
             // Calculate the corners of the menu
@@ -46,12 +53,12 @@ function pauseGame() {
                 var choose = Math.floor(x / 90) + 3*Math.floor(y / 90);
 
                 // Display the choice
-                chooseLabel.text = 'You chose menu item: ' + choosemap[choose];
+                // chooseLabel.text = 'You chose menu item: ' + choosemap[choose];
             }
             else{
                 // Remove the menu and the label
                 menu.destroy();
-                chooseLabel.destroy();
+                // chooseLabel.destroy();
 
                 // Unpause the game
                 this.game.paused = false;

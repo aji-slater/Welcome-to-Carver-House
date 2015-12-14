749f05f81fc7695a39e9e2309df1409d23be750d
[01;31m[K"name"[m[K: "Christopher \"Aji\" Slater <[01;31m[K"email"[m[K: null>
2015-12-14 17:01:18 -0600

    sets fade-in and fade-out of the inventory menu

--- js/hudDisplay.js
+++ js/hudDisplay.js
@@ -5,16 +5,28 @@ var hudDisplay = function(){
 
 var hudClick = function(){
   hudFrame.inputEnabled = true;
-  hudFrame.events.onInputDown.add(pause, this);
+  hudFrame.events.onInputDown.add(function(){
+    if (isPaused){
+      unPause();
+    } else {
+      pause();
+    }
+  }, this);
 };
 
 var pause = function(){
   popup = this.game.add.sprite(game.camera.width / 2, game.camera.height / 2, 'inventoryMenu', menuGroup);
+  popup.alpha = 0;
   popup.fixedToCamera = true;
   popup.anchor.setTo(0.5, 0.5);
   hudFrame.tint = 0x777777;
+  game.add.tween(popup).to( { alpha: 1 }, 120, Phaser.Easing.Linear.None, true, 0, 0, false);
   isPaused = true;
+};
 
-
-    // game.add.tween(hudFrame).to({ tint: 4 }, 200, Phaser.Easing.Quadratic.InOut, true);
+var unPause = function(){
+  isPaused = false;
+  var disappear = game.add.tween(popup).to( { alpha: 0 }, 120, Phaser.Easing.Linear.None, true, 0, 0, false);
+  disappear.onComplete.add(function(){ popup.kill(); }, this);
+  hudFrame.tint = 0xffffff;
 };

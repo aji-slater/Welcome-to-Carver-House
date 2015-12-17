2c0c975055de6d4698b74c2badf4464dbb60f675
[01;31m[K"name"[m[K: "Christopher \"Aji\" Slater <[01;31m[K"email"[m[K: null>
2015-12-17 15:00:44 -0600

    correctly pasted emptyNeeded function

--- js/FloorGen.js
+++ js/FloorGen.js
@@ -59,35 +59,22 @@ var generateTiles = function (){
             if ( board[yi][xi] === 1 ){
                 woodFloorTile = this.game.add.isoSprite(xx, yy, 0, 'woodTile', 0, floorGroup);
                 woodFloorTile.anchor.set(0.5, 0);
-            }
-            else {
-                // emptyTile = this.game.add.isoSprite(xx, yy, 0, 'emptySquare', 0, emptyGroup);
-                // emptyTile.anchor.set(0.5, 0);
-                // game.physics.isoArcade.enable(emptyTile);
-                // emptyTile.body.collideWorldBounds = true;
-                // emptyTile.body.moves = false;
-                // emptyTile.body.setSize(56, 56, 5);
+            } else {
+              if (needAnEmpty(xi, yi) ){
+                emptyTile = this.game.add.isoSprite(xx, yy, 0, 'emptySquare', 0, emptyGroup);
+                emptyTile.anchor.set(0.5, 0);
+                game.physics.isoArcade.enable(emptyTile);
+                emptyTile.body.collideWorldBounds = true;
+                emptyTile.body.moves = false;
+                emptyTile.body.setSize(56, 56, 5);
               }
+            }
             xx += TILE_POS;
         }
         xx = 0;
         yy += TILE_POS;
     }
-        // Debug Tiles at 0x0
-    // var i = 0
-    // for (xx = 0; xx < 9*TILE_POS; xx+= TILE_POS){
-    //     debugTile = this.game.add.isoSprite(xx, 0, 0, 'debugTile', i, floorGroup);
-    //     debugTile.anchor.set(0.5, 0)
-    //     i++;
-    // }
 
-    // var i = 0
-    // for (yy = 0; yy < 9*TILE_POS; yy+= TILE_POS){
-    //     debugTile = this.game.add.isoSprite(0, yy, 0, 'debugTile', i, floorGroup);
-    //     debugTile.anchor.set(0.5, 0)
-    //     i++;
-    // }
-    emptyFill();
 };
 
 var generateWalls = function(){
@@ -134,35 +121,22 @@ function decorateWalls(){
   return  0;
 }
 
-
-function emptyFill(){
-  var xx = 0;
-  var yy = 0;
-  for (var yi = 0; yi < board.length; yi++){
-    for (var xi = 0; xi < board[0].length; xi++){
-      if ( !isEmptyNeeded(xi, yi) ){
-        emptyTile = this.game.add.isoSprite(xTilePosition(xi), yTilePosition(yi), 0, 'emptySquare', 0, emptyGroup);
-        emptyTile.anchor.set(0.5, 0);
-        game.physics.isoArcade.enable(emptyTile);
-        emptyTile.body.moves = false;
-        emptyTile.body.setSize(56, 56, 5);
-      }
+var needAnEmpty = function(x, y){
+  if ( x === 0 || y === 0 ){
+    return true;
+  }
+  if ( x === board[0].length-1 || y === board.length-1 ){
+    return true;
+  }
+  if (board[y-1][x-1] === 0 &&
+    board[y-1][x]     === 0 &&
+    board[y-1][x+1]   === 0 &&
+    board[y][x-1]     === 0 &&
+    board[y][x+1]     === 0 &&
+    board[y+1][x-1]   === 0 &&
+    board[y+1][x]     === 0 &&
+    board[y+1][x+1]   === 0   ){
+      return false;
     }
-  }
-}
-
-function isEmptyNeeded(x, y){
-  if ( x === 0 || y === 0 || x === board[0].length - 1 || y === board.length - 1 ){
-    return true;
-  } else if ( board[y][x-1] === 1 ){
-    return true;
-  } else if ( board[y-1][x] === 1 ){
-    return true;
-  } else if ( board[y+1][x] === 1 ){
-    return true;
-  } else if ( board[y][x+1] === 1 ){
-    return true;
-  } else {
-    return false;
-  }
-}
+  return true;
+};

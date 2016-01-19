e87159e2169692154d1388eaa8515719339e52b1
[01;31m[K"name"[m[K: "Christopher \"Aji\" Slater <aji.slater@gmail.com>
2016-01-19 15:28:24 -0600

    adds debug loading % message

--- js/Preloader.js
+++ js/Preloader.js
@@ -9,17 +9,24 @@ BasicGame.Preloader.prototype = {
 	preload: function() {
 		this.stage.backgroundColor = '#ffffff';
 		this.loadingHouse = this.add.sprite(this.game.world.centerX, this.game.world.centerY - 50, 'loadingHouse');
-		this.loadingText = this.add.sprite(this.game.world.centerX, this.game.world.centerY+200, 'loadingText')
+		this.loadingText = this.add.sprite(this.game.world.centerX, this.game.world.centerY+200, 'loadingText');
 		this.loadingHouse.scale.setTo(0.6);
 		this.loadingHouse.anchor.setTo(0.5);
-
-
+		this.loadingText.anchor.setTo(0.5);
 		loadAll();
 
 	},
 
 	create: function() {
-		// this.state.start('MainMenu');
+		text = game.add.text(32, 32, 'Load', { fill: "#ffffff"});
 	},
+
+	fileComplete: function(progress, cacheKey, success, totalLoaded, totalFiles){
+		text.setText("Percent Complete: " + progress + "%");
+	},
+
+	loadComplete: function(){
+		this.state.start('MainMenu');
+	}
 
 };

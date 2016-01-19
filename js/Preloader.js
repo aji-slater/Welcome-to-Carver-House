427fbead5f84269902f143f8ae7e47dc5e25d1d1
[01;31m[K"name"[m[K: "Christopher \"Aji\" Slater <aji.slater@gmail.com>
2016-01-19 15:37:38 -0600

    corrects functioning of preloading debug tracker

--- js/Preloader.js
+++ js/Preloader.js
@@ -7,18 +7,26 @@ BasicGame.Preloader = function (game) {
 BasicGame.Preloader.prototype = {
 
 	preload: function() {
+
+	},
+
+	create: function() {
 		this.stage.backgroundColor = '#ffffff';
 		this.loadingHouse = this.add.sprite(this.game.world.centerX, this.game.world.centerY - 50, 'loadingHouse');
 		this.loadingText = this.add.sprite(this.game.world.centerX, this.game.world.centerY+200, 'loadingText');
 		this.loadingHouse.scale.setTo(0.6);
 		this.loadingHouse.anchor.setTo(0.5);
 		this.loadingText.anchor.setTo(0.5);
-		loadAll();
 
+		text = game.add.text(32, 32, 'Load', { fill: "#ffffff"});
+		this.start();
+		game.load.onLoadStart.add(this.loadStart, this);
+		game.load.onFileComplete.add(this.fileComplete, this);
+		game.load.onLoadComplete.add(this.loadComplete, this);
 	},
 
-	create: function() {
-		text = game.add.text(32, 32, 'Load', { fill: "#ffffff"});
+	loadStart: function(){
+
 	},
 
 	fileComplete: function(progress, cacheKey, success, totalLoaded, totalFiles){
@@ -27,6 +35,12 @@ BasicGame.Preloader.prototype = {
 
 	loadComplete: function(){
 		this.state.start('MainMenu');
+	},
+
+	start: function(){
+		loadAll();
+		game.load.start();
+
 	}
 
 };
